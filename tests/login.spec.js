b8849db5264178020b4056e9932cd15b7ae84a44
Diegoindio <dii.gomess@gmail.com>
2025-02-14 22:39:35 -0300

    Voltando ao Padrão PageObject

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,7 +5,6 @@ import { obterCodigo2FA } from '../support/db';
 import { LoginPage } from '../pages/LoginPage';
 import { DashPage } from '../pages/DashPage';
 
-import { LoginActions } from '../actions/LoginActions';
 
 test('Não deve logar quando o codigo de autenticacao e invalido', async ({ page }) => {
   
@@ -50,26 +49,4 @@ test('Deve acessar a conta do usuario', async ({ page }) => {
   expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
 });
 
-test('Deve acessar a conta do usuario 2', async ({ page }) => {
 
-  const loginActions = new LoginActions(page)
-
-  const usuario = {
-    cpf: '00000014141',
-    senha: '147258'
-  }
-
-  await loginActions.acessaPagina()
-  await loginActions.informaCpf(usuario.cpf)
-  await loginActions.informaSenha(usuario.senha)
-
-
-  await page.waitForTimeout(3000)
-  const code = await obterCodigo2FA()
-
-  await loginActions.informa2FA(code)
-
-  await page.waitForTimeout(2000)
-
-  expect(await loginActions.obterSaldo()).toHaveText('R$ 5.000,00')
-});
