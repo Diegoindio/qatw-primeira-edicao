8dabfafad1bed3087c19e15806230970c7ed7c72
Diegoindio <dii.gomess@gmail.com>
2025-02-14 22:37:30 -0300

    Padrão de Projeto Actions

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,8 +3,9 @@ import { test, expect } from '@playwright/test';
 import { obterCodigo2FA } from '../support/db';
 
 import { LoginPage } from '../pages/LoginPage';
-
 import { DashPage } from '../pages/DashPage';
+
+import { LoginActions } from '../actions/LoginActions';
 
 test('Não deve logar quando o codigo de autenticacao e invalido', async ({ page }) => {
   
@@ -47,4 +48,28 @@ test('Deve acessar a conta do usuario', async ({ page }) => {
   await page.waitForTimeout(2000)
 
   expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
+});
+
+test('Deve acessar a conta do usuario 2', async ({ page }) => {
+
+  const loginActions = new LoginActions(page)
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258'
+  }
+
+  await loginActions.acessaPagina()
+  await loginActions.informaCpf(usuario.cpf)
+  await loginActions.informaSenha(usuario.senha)
+
+
+  await page.waitForTimeout(3000)
+  const code = await obterCodigo2FA()
+
+  await loginActions.informa2FA(code)
+
+  await page.waitForTimeout(2000)
+
+  expect(await loginActions.obterSaldo()).toHaveText('R$ 5.000,00')
 });
