d0f05f1596cab68634c59f74c894169831c93b93
Diegoindio <dii.gomess@gmail.com>
2025-02-23 20:06:40 -0300

    Consumindo filas do Redis

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,6 +4,9 @@ import { obterCodigo2FA } from '../support/db';
 
 import { LoginPage } from '../pages/LoginPage';
 import { DashPage } from '../pages/DashPage';
+import { TIMEOUT } from 'dns';
+
+import { cleanJobs, getJob } from '../support/redis';
 
 
 test('Não deve logar quando o codigo de autenticacao e invalido', async ({ page }) => {
@@ -33,18 +36,20 @@ test('Deve acessar a conta do usuario', async ({ page }) => {
     cpf: '00000014141',
     senha: '147258'
   }
+  await cleanJobs()
 
   await loginPage.acessaPagina()
   await loginPage.informaCpf(usuario.cpf)
   await loginPage.informaSenha(usuario.senha)
 
+await page.getByRole('heading', {name: 'Verificação em duas etapas'})
+    .waitFor({timeout: 3000})
 
-  await page.waitForTimeout(3000)
-  const code = await obterCodigo2FA()
-
-  await loginPage.informa2FA(code)
-
-  await page.waitForTimeout(2000)
+    const code =  await getJob() 
+ // const code = await obterCodigo2FA(usuario.cpf)
+  
+ 
+ await loginPage.informa2FA(code)
 
   expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
 });
